9b9a31b54aab93803e7f82052ab50f24a5c441b3
MidnightTale <midnighttale24@gmail.com>
2024-05-13 04:30:29 +0700

    optimize

--- src/main/java/net/hynse/finvary/Finvary.java
+++ src/main/java/net/hynse/finvary/Finvary.java
@@ -1,25 +1,28 @@
 package net.hynse.finvary;
 
+import io.github.retrooper.packetevents.util.folia.FoliaScheduler;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.Objects;
 import java.util.Random;
 
 public final class Finvary extends JavaPlugin implements Listener {
-
+    public static Finvary instance;
     @Override
     public void onEnable() {
-        // Register event listener
+
+        instance = this;
         getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 
     @EventHandler
@@ -27,19 +30,22 @@ public final class Finvary extends JavaPlugin implements Listener {
         Entity entity = event.getEntity();
         if (entity instanceof Cod || entity instanceof Salmon ||
                 entity instanceof TropicalFish || entity instanceof PufferFish) {
-            if (entity instanceof LivingEntity) {
-                LivingEntity livingEntity = (LivingEntity) entity;
-                double newSize = getRandomSize(0.5, 2.3);
-                livingEntity.getAttribute(Attribute.GENERIC_SCALE).setBaseValue(newSize);
-                double newHealth = newSize * 6;
-                livingEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(newHealth);
-                livingEntity.setHealth(newHealth);
-            }
+                FoliaScheduler.getRegionScheduler().runDelayed(instance, entity.getLocation(), (o) -> {
+                    modifyEntityAttributes((LivingEntity) entity);
+                    },1);
         }
     }
-    private double getRandomSize(double minSize, double maxSize) {
+
+    private void modifyEntityAttributes(LivingEntity livingEntity) {
+        double newSize = getRandomSize();
+        Objects.requireNonNull(livingEntity.getAttribute(Attribute.GENERIC_SCALE)).setBaseValue(newSize);
+        double newHealth = newSize * 6;
+        Objects.requireNonNull(livingEntity.getAttribute(Attribute.GENERIC_MAX_HEALTH)).setBaseValue(newHealth);
+        livingEntity.setHealth(newHealth);
+    }
+
+    private double getRandomSize() {
         Random random = new Random();
-        return minSize + random.nextDouble() * (maxSize - minSize);
+        return 0.3 + random.nextDouble() * (1.6 - 0.7);
     }
 }
-
