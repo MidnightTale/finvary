78e444ccf6915fafed244debb0c99ad4911f29db
MidnightTale <midnighttale24@gmail.com>
2024-05-13 03:10:26 +0700

    test idea

--- src/main/java/net/hynse/finvary/Finvary.java
+++ src/main/java/net/hynse/finvary/Finvary.java
@@ -1,6 +1,13 @@
 package net.hynse.finvary;
 
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Cod;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Random;
 
 public final class Finvary extends JavaPlugin {
 
@@ -13,5 +20,26 @@ public final class Finvary extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+    @EventHandler
+    public void onEntitySpawn(EntitySpawnEvent event) {
+        if (event.getEntityType() == EntityType.COD) {
+            Cod cod = (Cod) event.getEntity();
+            //double randomHealth = getRandomHealth();
+            //cod.setHealth(randomHealth);
+            if (cod.getAttribute(Attribute.GENERIC_SCALE) != null) {
+                cod.getAttribute(Attribute.GENERIC_SCALE).setBaseValue(getRandomSize()
+                );
+            }
+        }
+    }
+
+//    private double getRandomHealth() {
+//        Random random = new Random();
+//        return random.nextDouble() * 5 + 5;
+//    }
+    private double getRandomSize() {
+        Random random = new Random();
+        return random.nextDouble() * 5+5;
     }
 }
