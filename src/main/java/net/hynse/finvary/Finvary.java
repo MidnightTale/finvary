aa1cf8360582b7193e37b6522f985156a96e36e4
MidnightTale <midnighttale24@gmail.com>
2024-05-13 02:51:58 +0700

    init

---
+++ src/main/java/net/hynse/finvary/Finvary.java
@@ -0,0 +1,17 @@
+package net.hynse.finvary;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Finvary extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
